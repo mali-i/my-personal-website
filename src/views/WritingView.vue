c8e46bca9e78924b827effea9c0f6fdddd5fc587
mali-i <hello_mali_i@163.com>
2025-09-05 17:54:00 +0800

    写作组件更新

--- src/views/WritingView.vue
+++ src/views/WritingView.vue
@@ -3,5 +3,19 @@
 </script>
 
 <template>
-    <p>这里是我的写作文章</p>
+
+   <p>阶段复盘</p>
+   <!-- 在新页面打开-->
+   <a href="https://mp.weixin.qq.com/s/3f2I_CvgFLpcO5Cpy7njow" >2025.08 熵增曲线波浪式地前进着</a>
+   <a href="https://mp.weixin.qq.com/s?__biz=Mzg4NDY2OTUwMA==&mid=2247484221&idx=1&sn=5210df184838995b2e3c720ce0fe2cf9&chksm=cfb5ee6ef8c267789c995c3d17d667dea875596a49912f436fb5e3d3b29f7d6fd5e815b8c909&token=586495846&lang=zh_CN#rd" >25.7.27-8.2 零到五维思维模式、阶段回顾、关于目标与情绪的新发现</a>
+
+    
 </template>
+
+<style scoped>
+a{
+    display: block;
+    margin: 10px;
+    
+}
+</style>
